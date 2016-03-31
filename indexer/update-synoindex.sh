b9e43d7da0b3fe2c3c8e0780ccd050f41773616b
Jean <tho_jea@yahoo.fr>
2016-03-31 20:12:20 +0200

    New path to psql with DSM 6

--- indexer/update-synoindex.sh
+++ indexer/update-synoindex.sh
@@ -12,17 +12,17 @@
 # ---------------------------------------------------------------------
 # Copyright (C) 2015-01-16  CPVprogrammer
 #                           https://github.com/CPVprogrammer
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
@@ -34,7 +34,7 @@ notify_subtitles(){
 
     if [[ ! -d "$PUSHBULLET_DIR" ]]; then
         echo "Pushbullet script not found"
-	exit
+        exit
     fi
 
     TITLE="Nouveau fichier de sous-titres trouvé !"
@@ -49,11 +49,11 @@ set_environment(){
     ALL_EXT="ASF AVI DIVX FLV IMG ISO M1V M2P M2T M2TS M2V M4V MKV MOV MP4 MPEG4 MPE MPG MPG4 MTS QT RM TP TRP TS VOB WMV XVID"
 
     CONFIG_DIR=$(dirname $0)"/config"
-    
+
     if [[ ! -d "$CONFIG_DIR" ]]; then
         mkdir "$CONFIG_DIR"
     fi
-    
+
     FICH_CONF="$CONFIG_DIR/update-synoindex-conf.txt"
 
     if [[ ! -f "$FICH_CONF" ]]; then
@@ -75,6 +75,9 @@ none
     READ_EXT=0
     READ_TIME=0
     READ_USER=0
+
+    # Path to psql
+    PSQL_PATH="/bin/psql"
 }
 
 #---------------------------------------------
@@ -82,7 +85,7 @@ none
 #---------------------------------------------
 extension(){
     FICH_EXT=${FICH_MEDIA##*.}
-    
+
     #convert to uppercase the extension
     FICH_EXT=$(echo $FICH_EXT | tr 'a-z' 'A-Z')
 }
@@ -95,10 +98,10 @@ check_extension(){
     if echo "$ALL_EXT" | grep -q "$FICH_EXT"; then
         TREATABLE=1
     else
-	echo "File not treated, unsupported extension ($FICH_EXT)"
+    echo "File not treated, unsupported extension ($FICH_EXT)"
         TREATABLE=0
     fi
-    
+
     return "$TREATABLE"
 }
 
@@ -109,30 +112,30 @@ check_extension(){
 search_directory_DB(){
     PATH_MEDIA=${FICH_MEDIA%/*}
     PATH_MEDIA_1=$(echo $PATH_MEDIA | tr 'A-Z' 'a-z')
-    
+
     #replace "'" with "\'"
     PATH_MEDIA_SQL=${PATH_MEDIA_1//"'"/"\'"}
-	TOTAL=0
-	FIRST=1
-	CREATE_DIR=0
-	
-	while : ; do
-		TOTAL=`/usr/syno/pgsql/bin/psql mediaserver -U postgres -tA -c "select count(1) from directory where lower(path) like '%$PATH_MEDIA_SQL%'"`
+    TOTAL=0
+    FIRST=1
+    CREATE_DIR=0
 
-		if [ "$TOTAL" = 0 ]; then
-			if [ "$FIRST" = 1 ]; then
-				FIRST=0
-			else
-				PATH_MEDIA_1=${PATH_MEDIA_1%/*}
-			fi
-			CREATE_DIR=1
-		fi
+    while : ; do
+        TOTAL=`$PSQL_PATH mediaserver -U postgres -tA -c "select count(1) from directory where lower(path) like '%$PATH_MEDIA_SQL%'"`
 
-		PATH_MEDIA_SQL=${PATH_MEDIA_SQL%/*}
-		if [ -z $PATH_MEDIA_SQL ]; then
-			break
-		fi		
-	done
+        if [ "$TOTAL" = 0 ]; then
+            if [ "$FIRST" = 1 ]; then
+                FIRST=0
+            else
+                PATH_MEDIA_1=${PATH_MEDIA_1%/*}
+            fi
+            CREATE_DIR=1
+        fi
+
+        PATH_MEDIA_SQL=${PATH_MEDIA_SQL%/*}
+        if [ -z $PATH_MEDIA_SQL ]; then
+            break
+        fi
+    done
 
     return "$CREATE_DIR"
 }
@@ -147,7 +150,7 @@ search_file_DB(){
     #replace "'" with "\'"
     FICH_MEDIA_SQL=${FICH_MEDIA_1//"'"/"\'"}
 
-    TOTAL=`/usr/syno/pgsql/bin/psql mediaserver -U postgres -tA -c "select count(1) from video where lower(path) like '%$FICH_MEDIA_SQL%'"`
+    TOTAL=`$PSQL_PATH mediaserver -U postgres -tA -c "select count(1) from video where lower(path) like '%$FICH_MEDIA_SQL%'"`
 
     return "$TOTAL"
 }
@@ -168,8 +171,8 @@ add_directory_DB(){
 add_file_DB(){
     echo "Adding file $FICH_MEDIA to the database"
     if [ "$FICH_EXT" = "SRT" ]; then
-	BODY="$FICH_MEDIA"
-	notify_subtitles
+    BODY="$FICH_MEDIA"
+    notify_subtitles
     fi
     synoindex -a "$FICH_MEDIA"
 }
@@ -179,17 +182,17 @@ add_file_DB(){
 #function to treat directories
 #---------------------------------------------
 treat_directories(){
-	CREATE_FILE=1
-	
+    CREATE_FILE=1
+
     search_directory_DB
-	SEARCH_RETVAL=$?
-    
+    SEARCH_RETVAL=$?
+
     if [ "$SEARCH_RETVAL" == 1 ]; then
         add_directory_DB
-		CREATE_FILE=0
+        CREATE_FILE=0
     fi
-	
-	return $CREATE_FILE
+
+    return $CREATE_FILE
 }
 
 
@@ -200,21 +203,21 @@ treat_files(){
     echo "Analyzing $FICH_MEDIA"
     extension
     check_extension
-    
+
     EXT_RETVAL=$?
     if [ "$EXT_RETVAL" == 1 ]; then
         search_file_DB
         SEARCH_RETVAL=$?
-	echo "Status: $SEARCH_RETVAL"
-        
+    echo "Status: $SEARCH_RETVAL"
+
         if [ "$SEARCH_RETVAL" == 0 ]; then
             treat_directories
-			
-			EXT_RETVAL=$?
 
-			if [ "$EXT_RETVAL" == 1 ]; then
-				add_file_DB
-			fi
+            EXT_RETVAL=$?
+
+            if [ "$EXT_RETVAL" == 1 ]; then
+                add_file_DB
+            fi
         fi
     fi
 }
@@ -230,15 +233,15 @@ treatment(){
         #skip comment and blank lines
         case "$LINE" in \#*) continue ;; esac
         [ -z "$LINE" ] && continue
-        
+
         #read the extensions from file
         if [[ "$READ_EXT" -eq 0 ]]; then
-            
+
             ALL_EXT=$LINE
-            
+
             #convert to uppercase
             ALL_EXT=$(echo $ALL_EXT | tr 'a-z' 'A-Z')
-            
+
             READ_EXT=1
             continue
         fi
@@ -246,13 +249,13 @@ treatment(){
         #read the update time from file
         if [[ "$READ_TIME" -eq 0 ]]; then
             LINE=$(echo $LINE | tr 'A-Z' 'a-z')
-            
+
             if [ "$LINE" == "none" ]; then
                 TIME_UPD=""
             else
                 TIME_UPD="$LINE"
             fi
-            
+
             READ_TIME=1
             continue
         fi
@@ -260,21 +263,21 @@ treatment(){
         #read the user from file
         if [[ "$READ_USER" -eq 0 ]]; then
             LINE=$(echo $LINE | tr 'A-Z' 'a-z')
-            
+
             if [ "$LINE" == "none" ]; then
                 USER_OWN=""
             else
                 USER_OWN="-user $LINE"
             fi
-            
+
             READ_USER=1
             continue
         fi
-        
+
         #read the paths from file
         RECURSIVE=$(echo $LINE | awk -F" " '{print $1}')
         PATH_FILE=$(echo $LINE | awk -F" " '{print $2}')
-        
+
         #delete last / if exists
         PATH_FILE="${PATH_FILE%/}"
 
@@ -285,7 +288,7 @@ treatment(){
             #no recursive find
             RECURSIVE="-maxdepth $RECURSIVE"
         fi
-        
+
         PARAMETERS="$PATH_FILE $RECURSIVE $TIME_UPD -type f $USER_OWN"
 
         find $PARAMETERS |
